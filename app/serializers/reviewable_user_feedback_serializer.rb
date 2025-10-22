1215fabd0e0db0f4fed8efebdf5ae236b3b5845a
kjm <kjmesu@protonmail.com>
2025-10-22 14:05:33 +0000

    feat(reviewable-user-feedback): Add payload attributes to serializer and details to connector

--- app/serializers/reviewable_user_feedback_serializer.rb
+++ app/serializers/reviewable_user_feedback_serializer.rb
@@ -1,17 +1,37 @@
 # frozen_string_literal: true
 
 class ReviewableUserFeedbackSerializer < ReviewableSerializer
-  attributes :type
+  attributes :type, :feedback_id, :user_id_val, :feedback_to_id, :rating, :review, :reason, :message
 
   def type
     'ReviewableUserFeedback'
   end
 
-  # Explicitly include payload data
-  def payload
-    object.payload
+  def feedback_id
+    object.payload['feedback_id'] rescue nil
   end
 
-  # Define what attributes should be in the payload
-  payload_attributes :feedback_id, :user_id, :feedback_to_id, :rating, :review, :reason, :message
+  def user_id_val
+    object.payload['user_id'] rescue nil
+  end
+
+  def feedback_to_id
+    object.payload['feedback_to_id'] rescue nil
+  end
+
+  def rating
+    object.payload['rating'] rescue nil
+  end
+
+  def review
+    object.payload['review'] rescue nil
+  end
+
+  def reason
+    object.payload['reason'] rescue nil
+  end
+
+  def message
+    object.payload['message'] rescue nil
+  end
 end
