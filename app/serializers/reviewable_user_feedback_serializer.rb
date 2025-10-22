12e4633fd6bf429fdc47f39c5814c5cf45a7e44e
kjm <kjmesu@protonmail.com>
2025-10-22 13:59:05 +0000

    feat(reviewable-user-feedback): Add type attribute to serializer
    This adds the `type` attribute to the `ReviewableUserFeedbackSerializer`.
    It also explicitly includes the `payload` data.

--- app/serializers/reviewable_user_feedback_serializer.rb
+++ app/serializers/reviewable_user_feedback_serializer.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
 class ReviewableUserFeedbackSerializer < ReviewableSerializer
+  attributes :type
+
+  def type
+    'ReviewableUserFeedback'
+  end
+
+  # Explicitly include payload data
+  def payload
+    object.payload
+  end
+
+  # Define what attributes should be in the payload
   payload_attributes :feedback_id, :user_id, :feedback_to_id, :rating, :review, :reason, :message
 end
