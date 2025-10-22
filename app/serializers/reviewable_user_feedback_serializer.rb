e9e91b9636f59eb810e6cbf90b2d796e35daf675
kjm <kjmesu@protonmail.com>
2025-10-22 20:17:42 +0000

    feat(user_feedbacks): Render reviewable user feedback with new component

--- app/serializers/reviewable_user_feedback_serializer.rb
+++ app/serializers/reviewable_user_feedback_serializer.rb
@@ -2,4 +2,14 @@
 
 class ReviewableUserFeedbackSerializer < ReviewableSerializer
   payload_attributes :feedback_id, :user_id, :feedback_to_id, :rating, :review, :reason, :message
+
+  attributes :target_created_by
+
+  def target_created_by
+    BasicUserSerializer.new(object.target&.user, scope: scope, root: false).as_json
+  end
+
+  def include_target_created_by?
+    object.target&.user.present?
+  end
 end
