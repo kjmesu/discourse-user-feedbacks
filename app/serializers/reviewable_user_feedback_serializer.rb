6d4c79f5bc0528b0c4b80030e83051349933d7a7
kjm <kjmesu@protonmail.com>
2025-10-22 00:15:54 +0000

    feat: add reviewable template and serializer enhancements for user feedback
    - Add target_created_by and target_feedback_to attributes to serializer
    - Create reviewable-user-feedback.hbs template with feedback details
    - Add styling for reviewable feedback display with rating and review info

--- app/serializers/reviewable_user_feedback_serializer.rb
+++ app/serializers/reviewable_user_feedback_serializer.rb
@@ -4,6 +4,20 @@ class ReviewableUserFeedbackSerializer < ReviewableSerializer
   target_attributes :id, :user_id, :feedback_to_id, :rating, :review, :created_at
   payload_attributes :feedback_id, :user_id, :feedback_to_id, :rating, :review, :reason, :message
 
+  attributes :target_created_by, :target_feedback_to
+
+  def target_created_by
+    if object.target&.user
+      BasicUserSerializer.new(object.target.user, root: false).as_json
+    end
+  end
+
+  def target_feedback_to
+    if object.target&.feedback_to
+      BasicUserSerializer.new(object.target.feedback_to, root: false).as_json
+    end
+  end
+
   def created_by_user
     BasicUserSerializer.new(object.created_by, root: false).as_json
   end
