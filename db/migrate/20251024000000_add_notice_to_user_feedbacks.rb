9bac3a87d9e7972489c20ffcfb48533a5843a2ce
kjm <kjmesu@protonmail.com>
2025-10-23 21:30:25 +0000

    refactor(db): Rename user_feedbacks table to discourse_user_feedbacks

--- db/migrate/20251024000000_add_notice_to_user_feedbacks.rb
+++ db/migrate/20251024000000_add_notice_to_user_feedbacks.rb
@@ -2,9 +2,9 @@
 
 class AddNoticeToUserFeedbacks < ActiveRecord::Migration[7.0]
   def change
-    add_column :user_feedbacks, :notice, :jsonb
-    add_column :user_feedbacks, :notice_created_by_id, :integer
+    add_column :discourse_user_feedbacks, :notice, :jsonb
+    add_column :discourse_user_feedbacks, :notice_created_by_id, :integer
 
-    add_index :user_feedbacks, :notice_created_by_id
+    add_index :discourse_user_feedbacks, :notice_created_by_id
   end
 end
