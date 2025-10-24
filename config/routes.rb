819330a0152d2373c4479bfb9e442461de21e2c6
kjm <kjmesu@protonmail.com>
2025-10-24 00:42:24 +0000

    feat(routes): Add notice action to user_feedbacks resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 DiscourseUserFeedbacks::Engine.routes.draw do
-  put 'user_feedbacks/:id/notice' => 'user_feedbacks#notice', constraints: DiscourseUserFeedbacks::UserFeedbacksConstraint.new
-
   resources :user_feedbacks, constraints: DiscourseUserFeedbacks::UserFeedbacksConstraint.new do
     member do
       get :show
       post :flag
       put :recover
       put :unhide
+      put :notice
     end
   end
 end
