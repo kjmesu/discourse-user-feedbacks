53b4551621197fa42c0562eaab6fedeb6e9f8699
kjm <kjmesu@protonmail.com>
2025-10-25 00:22:07 +0000

    feat: Allow access to user feedbacks based on site settings

--- lib/discourse_user_feedbacks/user_feedbacks_constraint.rb
+++ lib/discourse_user_feedbacks/user_feedbacks_constraint.rb
@@ -2,17 +2,16 @@
 
 class DiscourseUserFeedbacks::UserFeedbacksConstraint
   def matches?(request)
-    # Always allow non-GET requests (POST, PUT, DELETE)
     return true if request.request_method != "GET"
 
-    # For GET requests, just check if user is logged in
     current_user = CurrentUser.lookup_from_env(request.env)
 
-    # Allow if user is logged in
-    return true if current_user.present?
+    return true if !SiteSetting.user_feedbacks_hide_feedbacks_from_user
 
-    # Block anonymous users
-    false
+    return false if !current_user
+    return false if request.query_parameters["feedback_to_id"].to_i == current_user.id && current_user.admin == false
+
+    true
   rescue Discourse::InvalidAccess, Discourse::ReadOnly
     false
   end
